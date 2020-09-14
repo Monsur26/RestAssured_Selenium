fbbe6564ee602eb351ea64d2eed02dc2bf0e5501
Monsur26 <monsur.ahmed26@gmail.com>
2020-09-13 23:03:59 -0400

    first commit

---
+++ src/test/java/TC01_GET_Request.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC01_GET_Request {
+    @Test
+    void getWeatherDetail(){
+        //Specifying Base URI
+        RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city";
+        //Request Object
+        RequestSpecification httpRequest=RestAssured.given();
+        //Response Object
+        Response response=httpRequest.request(Method.GET,"/Hyderabad");
+        //print response
+        String responseBody=response.getBody().asString();
+        System.out.println("Response Body is : "+ responseBody);
+        //status code validation
+        int statusCode=response.getStatusCode();
+        System.out.println("Status code is: "+statusCode);
+        Assert.assertEquals(statusCode, 200);
+
+        //status line verification
+        String statusLine=response.getStatusLine();
+        System.out.println("Status line is:"+statusLine);
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+    }
+}
